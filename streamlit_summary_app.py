bd8145de982c889f9c0c69b2105d6b5c95073b6d
Mikel <mxagar@gmail.com>
2023-02-05 15:55:27 +0100

    add file upload

--- streamlit_summary_app.py
+++ streamlit_summary_app.py
@@ -42,6 +42,7 @@ List of concepts:
     - Columns
     - Tabs
     - Grouping user input in functions
+- File uploading
 
 General notes:
 
@@ -61,6 +62,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 import base64
+import pickle
 
 # Load page/app logo
 image = Image.open('logo.jpg')
@@ -260,3 +262,47 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
     # Then we pass features to a model for prediction!
+
+## Upload a file with dataset / Use user input from sidebar
+# The following snippet tries to upload a CSV from the user (and provides an example CSV as a link)
+# If the CSV is not uploaded by the user, the input from the sliders is taken
+# Example: app_8_classification_penguins
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+""")
+# Collects user input features into dataframe
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    # No file uploaded, take user input from sliders
+    def user_input_features():
+        # Penguins dataset
+        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+        sex = st.sidebar.selectbox('Sex',('male','female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+        data = {'island': island,
+                'bill_length_mm': bill_length_mm,
+                'bill_depth_mm': bill_depth_mm,
+                'flipper_length_mm': flipper_length_mm,
+                'body_mass_g': body_mass_g,
+                'sex': sex}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+
+## Load model pickle + perform inference
+# Example: app_8_classification_penguins
+# Reads in saved classification model
+load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+# Apply model to make predictions
+prediction = load_clf.predict(df)
+prediction_proba = load_clf.predict_proba(df)
+st.subheader('Prediction')
+penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+st.write(penguins_species[prediction])
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
